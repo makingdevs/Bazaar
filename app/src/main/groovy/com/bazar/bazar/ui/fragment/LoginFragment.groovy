834670e18d0a4e00889bf927113dc7ecd2a0329d
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-28 17:16:10 -0500

    #1 agregando el inicio del nuevo init del activity

--- app/src/main/groovy/com/bazar/bazar/ui/fragment/LoginFragment.groovy
+++ app/src/main/groovy/com/bazar/bazar/ui/fragment/LoginFragment.groovy
@@ -1,5 +1,6 @@
 package com.bazar.bazar.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
@@ -8,13 +9,16 @@ import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.bazar.bazar.R
+import com.bazar.bazar.ui.activity.PrincipalActivity
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class LoginFragment extends Fragment{
 
-    private static final String TAG = "LoginFragment"
+    static final String TAG = "LoginFragment"
+    LinearLayout mButtonLogin
 
     LoginFragment(){}
 
@@ -22,6 +26,12 @@ class LoginFragment extends Fragment{
         View root = inflater.inflate(R.layout.fragment_login, container, false)
         Toolbar toolbar = (Toolbar) root.findViewById(R.id.toolbar)
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar)
+        mButtonLogin = (LinearLayout) root.findViewById(R.id.login_button)
+        mButtonLogin.onClickListener = {
+            Intent intent = PrincipalActivity.newIntentWithContext(getContext())
+            startActivity(intent)
+            getActivity().finish()
+        }
         root
     }
 
