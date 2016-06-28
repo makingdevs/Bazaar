65c6b8107cb5e7f16046580d12e1831db1e90017
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-28 17:21:23 -0500

    #1 implementando la manejador para user y sus peticiones

---
+++ app/src/main/groovy/com/bazar/bazar/manager/UserManagerImpl.groovy
@@ -0,0 +1,20 @@
+package com.bazar.bazar.manager
+
+import com.bazar.bazar.network.UserRestOperations
+import com.bazar.bazar.network.impl.RetrofitTemplate
+import groovy.transform.CompileStatic
+
+@Singleton
+@CompileStatic
+class UserManagerImpl implements UserManager {
+
+    static operations = UserRestOperations
+
+    @Override
+    void list(Map params, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ UserRestOperations restOperations ->
+            restOperations.getUsers(params)
+        }
+    }
+
+}
