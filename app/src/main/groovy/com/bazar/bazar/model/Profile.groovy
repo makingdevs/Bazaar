aa0a7d07dcdebcf7a156440f29f4545b91facaa7
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-28 17:21:41 -0500

    #1 creando model profile esto para el json de respuesta

---
+++ app/src/main/groovy/com/bazar/bazar/model/Profile.groovy
@@ -0,0 +1,13 @@
+package com.bazar.bazar.model
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Profile {
+    String picture
+    String hometown
+    String location
+    String status
+    String gender
+    String name
+}
